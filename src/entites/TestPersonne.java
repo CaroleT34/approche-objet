e8dc05530b4339d04e0a6ef0513a2003595d3af4
Carole TOULORGE <carole.toulorge@hotmail.fr>
2022-10-11 11:00:52 +0200

    Corrigé TP3

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -3,23 +3,22 @@ package entites;
 import entites2.Personne;
 
 public class TestPersonne {
-	
-	public static String prenom;
-	public static String nom;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Personne Personne1 = new Personne("carole", "toulorge");
-		prenom = Personne1.modifPrenom("carole");
-		nom = Personne1.modifNom("toulorge");
-		
 		AdressePostale adresse2 = new AdressePostale(5, "Rue des Jonquilles", 33160, "Saint Médard en Jalles");
+		
+		Personne Personne1 = new Personne("carole", "toulorge");
 		Personne Personne2 = new Personne("Jasmine", "Ali", adresse2);
-		prenom = Personne2.modifPrenom("JasmiNE");
-		nom = Personne2.modifNom("Ali");
+		
+		Personne1.afficherIdentite();
+		Personne2.afficherIdentite();
+		
+		Personne1.setNom("Jolly");
+		Personne2.setPrenom("aBou");
+		
+		Personne1.afficherIdentite();
+		Personne2.afficherIdentite();
 
-		System.out.println(" Information de la personne 1 : " + prenom + " " + nom);
-		System.out.println(" Information de la personne 2 : " + Personne2.prenom + " " + Personne2.nom.toUpperCase() + " " + adresse2);
 	}
 
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -4,40 +4,72 @@ import entites.AdressePostale;
 
 public class Personne {
 	
-	public String prenom;
-	public String nom;
-	public AdressePostale adresse;
+	//ATTRIBUT D'INSTANCE
+	private String prenom;
+	private String nom;
+	private AdressePostale adresse;
 	
-	//Modif le nom
-	public String modifNom(String nom) {
-		
-		return nom.toUpperCase();
-	}
-	
-	//Modif le prenom
-	public String modifPrenom(String prenom) {
-		
-		return prenom.substring(0, 1).toUpperCase()+ "" + prenom.substring(1).toLowerCase();
-	}
-	
-	//constructeur
+	//CONSTRUCTEUR
 	public Personne(String prenom, String nom) {
 		this.prenom = prenom;
 		this.nom = nom;
 		
-		System.out.println(" Information : " + modifPrenom(prenom) + " " + modifNom(nom));
+	}
+	
+	public Personne(String prenom, String nom, AdressePostale adresse) {
+		this.prenom = prenom;
+		this.nom = nom;
+		this.adresse = adresse;
 		
 	}
 	
-	//constructeur
-		public Personne(String prenom, String nom, AdressePostale adresse) {
-			this.prenom = prenom;
-			this.nom = nom;
-			this.adresse = adresse;
-			
-			System.out.println(" Information : " + modifPrenom(prenom) + " " + modifNom(nom) + " " + adresse);
-			
+	//METHODES
+	//Afficher nom et prenom
+	public void afficherIdentite() {
+		if (adresse ==null) {
+			System.out.println(" Identité : " + prenom.substring(0, 1).toUpperCase()+ "" + prenom.substring(1).toLowerCase() + " " + nom.toUpperCase());
+		} else {
+			System.out.println(" Identité : " + prenom.substring(0, 1).toUpperCase()+ "" + prenom.substring(1).toLowerCase() + " " + nom.toUpperCase() + " " + adresse);
 		}
+	}
+	
+	//GETTER retourne un résultat
+	// 1 par Attribut d'instance
+	//NOM
+	public String getNom(String nom) {
+		return nom;
+	}
+	
+	//PRENOM
+	public String getPrenom(String prenom) {
+		return prenom;
+	}
+	
+	//ADRESSE
+	public AdressePostale getAdresse(AdressePostale adresse) {
+		return adresse;
+	}
+	
+	//Setteur modifie un résultat
+	// 1 par Attribut d'instance
+	//NOM
+	public void setNom(String nom) {
+		this.nom = nom ;
+	}
+	
+	//PRENOM
+	public void setPrenom(String prenom) {
+		this.prenom = prenom ;
+	}
+	
+	//ADRESSE
+	public void setAdresse(AdressePostale adresse) {
+		this.adresse = adresse ;
+	}
+		
+	
+	
+	
 	
 	
 }
