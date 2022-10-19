0cc08ea88e0b6a29796de09e80eb99b6cd73fd7c
Carole TOULORGE <carole.toulorge@hotmail.fr>
2022-10-19 11:35:46 +0200

    TP 1 - COMPRENDRE L’INTERET DE LA CLASSE STRINGBUILDER

---
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -0,0 +1,30 @@
+package fr.diginamic.chaines;
+
+public class TestStringBuilder {
+	public static void main(String[] args) {
+		
+		long debut = System.currentTimeMillis(); // Code à chronométrer 
+		
+		StringBuilder builder = new StringBuilder();
+		for (int i = 1; i <= 100000; i++) {
+			builder = builder.append(i);
+		}
+		
+		//System.out.println(builder);
+		long fin = System.currentTimeMillis(); 
+		
+		System.out.println("Temps écoulé en millisecondes :" + (fin - debut));
+		
+		long debutTest = System.currentTimeMillis(); // Code à chronométrer 
+		
+		String test = new String();
+		for (int i = 1; i <= 100000; i++) {
+			test += i;
+		}
+		
+		//System.out.println(builder);
+		long finTest = System.currentTimeMillis(); 
+		
+		System.out.println("Temps écoulé en millisecondes :" + (finTest - debutTest));
+	}
+}
